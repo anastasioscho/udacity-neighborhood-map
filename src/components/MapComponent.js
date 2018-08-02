4e2b97a1fc1c8a695b5e7b1fbe8a344337d2382a
Anastasios Chondrogiannis <info@anastasioscho.gr>
2018-08-02 21:49:58 +0300

    feat: Show the map

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import loadjs from 'loadjs'
 
 class MapComponent extends Component {
+    componentDidMount() {
+        loadjs('https://maps.googleapis.com/maps/api/js?key=AIzaSyC4j2OsSiIGHMKVka01EsmJV6VG88XwvG4', 'load-google-maps-api');
+        loadjs.ready('load-google-maps-api', () => {
+            const map = new window.google.maps.Map(this.refs.map, {
+                center: {lat: 40.696855, lng: 24.656471},
+                zoom: 11
+            });
+            this.setState({map});
+        });
+    }
+
     render() {
         return (
-            <p>This is where the map goes</p>
+            <div ref="map" role="application" aria-label='A map showing the locations' style={{height: '100vh', width: '100%'}}>This is where the map goes</div>
         )
     }
 }
