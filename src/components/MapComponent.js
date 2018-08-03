b55b491986141d27999eaaf0ef7b863440ce98c4
Anastasios Chondrogiannis <info@anastasioscho.gr>
2018-08-03 17:44:59 +0300

    refactor: Simplify the way the map component works

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -3,56 +3,32 @@ import loadjs from 'loadjs'
 import PropTypes from 'prop-types'
 
 class MapComponent extends Component {
-    state = {
-        markers: []
-    }
-
     componentDidMount() {
         loadjs('https://maps.googleapis.com/maps/api/js?key=AIzaSyC4j2OsSiIGHMKVka01EsmJV6VG88XwvG4', 'load-google-maps-api');
         loadjs.ready('load-google-maps-api', () => {
-            const map = new window.google.maps.Map(this.refs.map, {
+            this.map = new window.google.maps.Map(this.refs.map, {
                 center: {lat: 40.696855, lng: 24.656471},
                 zoom: 11
             });
 
-            const markers = this.markersFromLocations(this.props.locations);
-            this.addMarkersToMap(markers, map);
-
-            this.setState({map, markers});
+            this.markers = this.markersFromLocations(this.props.locations);
+            this.addMarkersToMap(this.markers, this.map);
         });
     }
 
     shouldComponentUpdate(nextProps) {
-        return (this.props !== nextProps);
-    }
+        this.clearMarkersFromMap(this.markers);
+        this.markers = this.markersFromLocations(nextProps.locations);
+        this.addMarkersToMap(this.markers, this.map);
 
-    componentDidUpdate(prevProps) {
-        const markers = this.updatedMarkers(prevProps);
-
-        if (this.state.map && markers) {
-            this.clearMarkersFromMap(this.state.markers);
-            this.addMarkersToMap(markers, this.state.map);
-            this.setState({markers});
-        }
+        return false;
     }
 
     render() {
+        console.log('MapComponent', 'I just rendered');
         return (
             <div ref="map" role="application" aria-label='A map showing the locations' style={{height: '100vh', width: '100%'}}>This is where the map goes</div>
         )
-    }
-
-    updatedMarkers(prevProps) {
-        // Returns a new set of markers if the locations are different
-
-        const currentLocations = this.props.locations;
-        const previousLocations = prevProps.locations;
-
-        if (currentLocations === previousLocations && this.state.markers.length > 0) {
-            return undefined
-        }
-
-        return this.markersFromLocations(currentLocations);
     }
 
     /* Helper Functions */
